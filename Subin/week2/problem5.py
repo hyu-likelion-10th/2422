0998c8291605f2e62815b44dac8ed9c4d5c15517
LeeSubin <supiao0123@hanyang.ac.kr>
2022-05-08 23:59:51 +0900

    fix: fix number 5

--- Subin/week2/problem5.py
+++ Subin/week2/problem5.py
@@ -1,5 +1,12 @@
+
 def solution(phone_book):
-    for i in phone_book:
-        
+    counter = 0
     answer = True
+    for i in phone_book:
+        for j in phone_book[counter + 1:]:
+            if j.startswith(i):
+                answer = False
+                return answer
+        counter += 1
+    
     return answer
