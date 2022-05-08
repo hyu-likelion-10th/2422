c8beda47ff7834b1496868619d0acdec21d9ef69
LeeSubin <supiao0123@hanyang.ac.kr>
2022-05-09 00:58:09 +0900

    fix: improve efficiency of problem5.py

--- Subin/week2/problem5.py
+++ Subin/week2/problem5.py
@@ -1,12 +1,18 @@
-
 def solution(phone_book):
-    counter = 0
     answer = True
-    for i in phone_book:
-        for j in phone_book[counter + 1:]:
-            if j.startswith(i):
-                answer = False
-                return answer
-        counter += 1
-    
+    phone_book.sort()
+    for i in range(0, len(phone_book) - 1):
+        if phone_book[i + 1].startswith(phone_book[i]):
+            answer = False
+            return answer
+
     return answer
+
+    # phone_book.sort(key=lambda x: len(x))
+    # phone_book = sorted(phone_book, key=lambda x: len(x))
+    # for i in range(0, len(phone_book)):
+    #    for j in range(i + 1, len(phone_book)):
+    #        if phone_book[i] != phone_book[j]:
+    #            if phone_book[j].startswith(phone_book[i]):
+    #                answer = False
+    #                return answer
